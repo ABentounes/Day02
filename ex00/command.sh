f13a2d4343ba6bd77e8fbd336a9d2b675e4a65ff
ABentounes <a.bentounes@it-students.fr>
2021-10-20 16:15:25 +0200

    day02

---
+++ ex00/command.sh
@@ -0,0 +1,5 @@
+mkdir node_modules/ pages/ public/ styles/ package.json package-lock.json next.config.js
+touch package.json package-lock.json next.config.js
+
+
+ls | wc -l && ls | tee -a ./sorted.txt > /dev/null
